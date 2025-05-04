99211549a3eb9890868280231e6a25775d94a811
Ali Fayed <alifayed.h@gmail.com>
2025-05-03 18:34:50 -0700

    [minor]: Added firebaseID to User model

--- src/models/user_model.js
+++ src/models/user_model.js
@@ -1,6 +1,11 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
+    firebase_id: {
+        type: String,
+        required: true,
+        unique: true
+    },
     email: {
         type: String,
         required: true,
